01bfb0430a208191fde013172400f698d4d0e9ac
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-06-25 19:56:31 +0200

    odpowiedzi

---
+++ src/Zad11x3.java
@@ -0,0 +1,13 @@
+public class Zad11x3 {
+    public static void main(String[] args) {
+        System.out.println("Jaki interfejs muszą implementować klasy, których obiekty chcemy serializować?");
+        System.out.println("java.io.Serializable."+"\n");
+        System.out.println(
+                "Jeżeli zależy nam na szybkim dostępie do danych, to z jakiego typu listy skorzystamy?");
+        System.out.println("ArrayList"+"\n");
+        System.out.println("Jeżeli mamy zamiar często zmieniać/wstawiać/usuwać elementy w środku listy lub na jej początku to z jakiej listy lepiej skorzystać?");
+        System.out.println("polaczonej LinkedList");
+    }
+
+
+}
